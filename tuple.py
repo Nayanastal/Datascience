0528569e28a90acc04d0d00adeb72db33438e85b
Nayanastal <9listal@gmail.com>
2025-09-18 14:58:51 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -3,3 +3,5 @@ tuple = (('Rajesh', 75),('Vasudevan', 80), ('Thomas', 85))
 print("Tuple=",tuple)
 dictionary = dict(tuple)
 print("Dictionary=",dictionary)
+
+https://vasudevantv.github.io/datasets.html
